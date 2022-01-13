a801b2277d45bf23f42e04246c96ae29f47f5ba5
bkrayoub <92027292+bkrayoub@users.noreply.github.com>
2022-01-13 17:02:45 +0100

    new api

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
 var searchInput = document.getElementById("trans");
 var moResult = document.getElementsByClassName("movies");
-var search = "ha";
+var search = "hell";
 fetch("https://imdb8.p.rapidapi.com/auto-complete?q=" + search, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "imdb8.p.rapidapi.com",
-		"x-rapidapi-key": "652492f9bcmshf24c46601b86b30p13088cjsn8b31ba67afaf"
+		"x-rapidapi-key": "0be463ade2mshcff878ea173edc7p1cd779jsn32841bb5fd95"
 	}
 })
 .then(response => response.json())
 .then(data => {
 	const lista = data.d
 	console.log(data)
-	lista.map((item) => {	
+	lista.map((item) => {
+		
 		const moName = item.l;
 		const poster = item.i.imageUrl;
 		const movie = `<li><img src="${poster}"><h3>${moName}</h3></li>`;
 		document.querySelector('.movies').innerHTML += movie;
+
 	})
 })
 .catch(err => {
 	console.error(err);
 });
 function btn(){
-	alert("");
+
 
 }
