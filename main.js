338cf70b48909a7a9072675de1fd6e00b0d92528
bkrayoub <92027292+bkrayoub@users.noreply.github.com>
2022-01-13 13:01:52 +0100

    link data from fetch API, change input design, display data

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 var searchInput = document.getElementById("trans");
-var result = document.getElementById("result");
-
-fetch("https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr", {
+var moResult = document.getElementsByClassName("movies");
+var search = "ha";
+fetch("https://imdb8.p.rapidapi.com/auto-complete?q=" + search, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "imdb8.p.rapidapi.com",
-		"x-rapidapi-key": "2195fc1f9dmsh109f121195f64c0p10b5b4jsn5639135863dc"
+		"x-rapidapi-key": "652492f9bcmshf24c46601b86b30p13088cjsn8b31ba67afaf"
 	}
 })
-.then(response => response.jason())
-.then(data => console.log(data))
+.then(response => response.json())
+.then(data => {
+	const lista = data.d
+	console.log(data)
+	lista.map((item) => {	
+		const moName = item.l;
+		const poster = item.i.imageUrl;
+		const movie = `<li><img src="${poster}"><h3>${moName}</h3></li>`;
+		document.querySelector('.movies').innerHTML += movie;
+	})
+})
 .catch(err => {
 	console.error(err);
 });
+function btn(){
+	alert("")
+
+}
